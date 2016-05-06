5278bfa3340581fb2dcd98f9740a6e48f883f213
HaNaK0 <saturn-v@hotmail.com>
2016-05-06 11:33:43 +0200

    Added the base files for tiled loader

---
+++ TBSA/Source/TiledLoading/TiledLoader/TiledLoader.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "TiledLoader.h"
+
+
+void TiledLoader::Load(std::string aFilePath)
+{
+	
+}
---
+++ TBSA/Source/TiledLoading/TiledLoader/TiledLoader.h
@@ -0,0 +1,10 @@
+/*
+*Author:  Hampus Huledal
+*Date: 2016-05-06
+*/
+#include <string>
+
+namespace TiledLoader
+{
+	inline void Load(std::string aFilePath);
+}
