f15c558b31bd4b8297587dd47383c5bc7ba803f1
HaNaK0 <saturn-v@hotmail.com>
2016-05-10 12:21:41 +0200

    Added Tilesheets and removed tile maps which weren't used

---
+++ TBSA/Source/TiledLoading/TiledLoader/SpriteSheet/SpriteSheet.cpp
@@ -0,0 +1,45 @@
+﻿#include "stdafx.h"
+#include "SpriteSheet.h"
+#include "../../../Renderer/Rend/WrappedSprite.h"
+
+
+std::string SpriteSheet::ourFolder = "Sprites/SpriteSheets";
+
+SpriteSheet::SpriteSheet() : SpriteSheet("", CommonUtilities::Vector2f(0,0), "", 0)
+{
+
+}
+
+SpriteSheet::SpriteSheet(const std::string& aName,const CommonUtilities::Vector2f& aSubImageSize,const std::string& aFileType, unsigned int aFirstId)
+{
+	myName = aName;
+	mySize = aSubImageSize;
+	myFileFormat = aFileType;
+	myFirstId = aFirstId;
+}
+
+SpriteSheet::~SpriteSheet()
+{
+}
+
+void SpriteSheet::SetFolder(const std::string & aPathToAFolder)
+{
+	ourFolder = aPathToAFolder;
+}
+
+std::string SpriteSheet::GetPath() const
+{
+	return ourFolder + myName + myFileFormat;
+}
+
+WrappedSprite* SpriteSheet::CreateSprite(unsigned /*aImageId*/, bool /*aUniqueId*/)
+{
+	//Todo: Add code to extract Subimages
+
+	return new WrappedSprite();
+}
+
+unsigned SpriteSheet::GetFirstIndex()
+{
+	return myFirstId;
+}
