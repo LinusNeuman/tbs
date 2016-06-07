9c8e858f28ae7e0cc9fe67688e52f5542242a611
Marcus Holst <macke.holst@gmail.com>
2016-06-07 10:43:09 +0200

    Renamed functions to better represent what they do

--- TBSA/Source/GameObjects/GameObjects/Room/IsometricTile.cpp
+++ TBSA/Source/GameObjects/GameObjects/Room/IsometricTile.cpp
@@ -16,10 +16,10 @@ IsometricTile::IsometricTile(const CommonUtilities::Vector2f & aPosition)
 	myIsVisible = false;
 	myIsInEnemyFoV = false;
 	myAvailableDirections.Init(8);
-	for (size_t i = 1; i < 9; i++)
+	/*for (size_t i = 1; i < 9; i++)
 	{
 		myAvailableDirections.Add(static_cast<eDirection>(i * 10));
-	}
+	}*/
 }
 
 IsometricTile::~IsometricTile()
@@ -150,9 +150,9 @@ bool IsometricTile::GetInEnemyFov() const
 	return myIsInEnemyFoV;
 }
 
-void IsometricTile::RemoveAvailableDirection(eDirection aDirection)
+void IsometricTile::SetAvailableDirection(eDirection aDirection)
 {
-	myAvailableDirections.RemoveCyclic(aDirection);
+	myAvailableDirections.Add(aDirection);
 }
 
 VertexHandle IsometricTile::GetVertexHandle() const
