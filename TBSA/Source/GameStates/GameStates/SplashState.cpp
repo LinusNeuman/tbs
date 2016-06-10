69824a710b52c8ffca49dbab1a47ae4d9fa11b9d
Linus Neuman <linus@neuman.io>
2016-06-10 16:10:04 +0200

    Now uses 1920-space for splashscreen

--- TBSA/Source/GameStates/GameStates/SplashState.cpp
+++ TBSA/Source/GameStates/GameStates/SplashState.cpp
@@ -41,7 +41,7 @@ void Splash::Render()
 		(10-80.f * 0.5f) - ((mySprite->GetSize().y) * 10-80.f)
 	});*/
 
-	mySprite->Draw({SingletonDataHolder::GetTargetResolution().x  * 0.5f, SingletonDataHolder::GetTargetResolution().y * 0.5f });
+	mySprite->Draw({1920.f  * 0.5f, 1080.f * 0.5f });
 }
 
 bool Splash::Fade(const CU::Time& aDelta)
