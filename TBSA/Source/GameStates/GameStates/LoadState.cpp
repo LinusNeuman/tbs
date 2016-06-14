68917426ce4908909ba1bf43f246332e00bad8f3
Simon Skogsrydh <simon.skogsrydh.sp15@thegameassembly.com>
2016-06-14 12:11:21 +0200

    put real loading state art into game.

--- TBSA/Source/GameStates/GameStates/LoadState.cpp
+++ TBSA/Source/GameStates/GameStates/LoadState.cpp
@@ -5,8 +5,12 @@
 LoadState::LoadState(TiledData* someData)
 {
 	myDataptr = someData;
-	myPosition = CommonUtilities::Vector2f::Zero;
-	mySprite.Init("Sprites/Splash/Group.dds", false);
+	mySprite.Init("Sprites/LoadingScreen/loading.dds", false, CU::Vector4f::Zero, {0.5f, 0.5f});
+	myDoneSprite.Init("Sprites/LoadingScreen/pressAnyKey.dds", false, CU::Vector4f::Zero, { 0.5f, 0.5f });
+	myDotSprite.Init("Sprites/LoadingScreen/dot1.dds", false);
+
+	myTimer.SetAlarmTimeLength(0.5f);
+	myDotCounter = 1;
 }
 
 LoadState::~LoadState()
@@ -15,7 +19,7 @@ LoadState::~LoadState()
 
 eStackReturnValue LoadState::Update(const CU::Time& aDeltaTime, ProxyStateStack& aStateStack)
 {
-	if (myDataptr->myIsLoaded == true)
+	if (myDataptr->myIsLoaded == false)
 	{
 		if (IsometricInput::GetAnyKeyReleased() == true || IsometricInput::GetAnyMouseButtonReleased() == true)
 		{
@@ -24,7 +28,16 @@ eStackReturnValue LoadState::Update(const CU::Time& aDeltaTime, ProxyStateStack&
 	}
 	else
 	{
-		myPosition += CommonUtilities::Vector2f(100, 100) * aDeltaTime.GetSeconds();
+		if (myTimer.GetProgress() >= 1.f)
+		{
+			myTimer.Reset();
+			++myDotCounter;
+
+			if (myDotCounter > 3)
+			{
+				myDotCounter = 1;
+			}
+		}
 	}
 
 	
@@ -33,5 +46,17 @@ eStackReturnValue LoadState::Update(const CU::Time& aDeltaTime, ProxyStateStack&
 
 void LoadState::Draw() const 
 {
-	mySprite.Draw(myPosition);
+	if (myDataptr->myIsLoaded == false)
+	{
+		myDoneSprite.DrawWithNormalized({ 0.5f, 0.5f });
+	}
+	else
+	{
+		mySprite.DrawWithNormalized({ 0.5f, 0.5f });
+
+		for (unsigned short iDot = 0; iDot < myDotCounter; ++iDot)
+		{
+			myDotSprite.DrawWithNormalized({ 0.4f + (0.05f * FLOATCAST(iDot)), 0.6f });
+		}
+	}
 }
