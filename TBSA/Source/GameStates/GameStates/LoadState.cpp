43e44a6f2af0dd7eefb7e69a02e9e3efa34813be
Simon Skogsrydh <simon.skogsrydh.sp15@thegameassembly.com>
2016-06-14 12:19:26 +0200

    added background to loadingscreen.

--- TBSA/Source/GameStates/GameStates/LoadState.cpp
+++ TBSA/Source/GameStates/GameStates/LoadState.cpp
@@ -5,12 +5,13 @@
 LoadState::LoadState(TiledData* someData)
 {
 	myDataptr = someData;
-	mySprite.Init("Sprites/LoadingScreen/loading.dds", false, CU::Vector4f::Zero, {0.5f, 0.5f});
+	mySpriteLoading.Init("Sprites/LoadingScreen/loading.dds", false, CU::Vector4f::Zero, {0.5f, 0.5f});
 	myDoneSprite.Init("Sprites/LoadingScreen/pressAnyKey.dds", false, CU::Vector4f::Zero, { 0.5f, 0.5f });
 	myDotSprite.Init("Sprites/LoadingScreen/dot1.dds", false);
+	myBackGround.Init("Sprites/mainmenu.dds", false, { 0.f, 0.f, 1920.f, 1080 }, { 0.5f, 0.5f });
 
 	myTimer.SetAlarmTimeLength(0.5f);
-	myDotCounter = 1;
+	myDotCounter = 0;
 }
 
 LoadState::~LoadState()
@@ -19,7 +20,7 @@ LoadState::~LoadState()
 
 eStackReturnValue LoadState::Update(const CU::Time& aDeltaTime, ProxyStateStack& aStateStack)
 {
-	if (myDataptr->myIsLoaded == false)
+	if (myDataptr->myIsLoaded == true)
 	{
 		if (IsometricInput::GetAnyKeyReleased() == true || IsometricInput::GetAnyMouseButtonReleased() == true)
 		{
@@ -35,7 +36,7 @@ eStackReturnValue LoadState::Update(const CU::Time& aDeltaTime, ProxyStateStack&
 
 			if (myDotCounter > 3)
 			{
-				myDotCounter = 1;
+				myDotCounter = 0;
 			}
 		}
 	}
@@ -46,13 +47,15 @@ eStackReturnValue LoadState::Update(const CU::Time& aDeltaTime, ProxyStateStack&
 
 void LoadState::Draw() const 
 {
-	if (myDataptr->myIsLoaded == false)
+	myBackGround.DrawWithNormalized({ 0.5f, 0.5f });
+
+	if (myDataptr->myIsLoaded == true)
 	{
 		myDoneSprite.DrawWithNormalized({ 0.5f, 0.5f });
 	}
 	else
 	{
-		mySprite.DrawWithNormalized({ 0.5f, 0.5f });
+		mySpriteLoading.DrawWithNormalized({ 0.5f, 0.5f });
 
 		for (unsigned short iDot = 0; iDot < myDotCounter; ++iDot)
 		{
