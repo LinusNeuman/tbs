fab3bc9e6a01f404cd50ce809263706cc5a30caf
Marcus Holst <macke.holst@gmail.com>
2016-06-14 09:37:48 +0200

    Player can now attack enemies diagonals

--- TBSA/Source/GameObjects/Controllers/EnemyController.cpp
+++ TBSA/Source/GameObjects/Controllers/EnemyController.cpp
@@ -34,11 +34,6 @@ void EnemyController::PreTurn()
 	{
 		myEnemies[i]->Reset();
 	}
-
-	for (size_t i = 0; i < myEnemies.Size(); i++)
-	{
-		myFloor->SetDiagonals(myEnemies[i]->GetPosition(), 1.1f);
-	}
 }
 
 void EnemyController::Update(CommonUtilities::Time aDeltaTime)
@@ -321,7 +316,6 @@ void EnemyController::PostTurn()
 {
 	for (size_t i = 0; i < myEnemies.Size(); i++)
 	{
-		myFloor->SetDiagonals(myEnemies[i]->GetPosition(), 10000 * 10000);
 		myEnemies[i]->AfterTurn();
 	}
 }
