27c19491bded747f83b5e5969aa238cf7445147a
Yaoses <leonpayao@gmail.com>
2016-06-15 14:28:27 +0200

    Level select background and changed start text to avoid errors I think

--- TBSA/Source/GameStates/GameStates/PreGameState.cpp
+++ TBSA/Source/GameStates/GameStates/PreGameState.cpp
@@ -7,7 +7,7 @@ PreGameState::PreGameState() : myTextBox({ 1920.f / 2 - 250, 250.f }, { 1920.f, 
 	myTextBox.SetPosition({ 1920.f / 2 - 250, 250.f });
 	myTextBox.SetSize({500.f, 750.f});
 	myTextBox.SetLines(50);
-	myTextBox.AddText("\"Alright, soldiers\"”, said Curly.");
+	myTextBox.AddText("\"Alright, soldiers\", said Curly.");
 	myTextBox.AddText("\"The first sign of winter has been spotted. Yesterday I saw Nutty Meg wear the same ugly mittens as every year, so the time has come to prepare\".");
 	myTextBox.AddText("Gathered around a crude map of the town, the three of them laid out their battle plan.They would have to lay claim to several key points in town before they could be named the winners of the annual snow-war.");
 	myTextBox.AddText("Beneath the treehouse in Curly's backyard they would hone their skills, Ginger and Long John the footsoldiers on ground, and Curly as their ever watchful commander.");
