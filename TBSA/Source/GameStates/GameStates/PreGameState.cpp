07fde8f521fdebccfd7b180e6ee4dd4f8efadf83
Simon Skogsrydh <simon.skogsrydh.sp15@thegameassembly.com>
2016-06-15 11:26:52 +0200

    pregamestate can now be skipped by clicking on anything.

--- TBSA/Source/GameStates/GameStates/PreGameState.cpp
+++ TBSA/Source/GameStates/GameStates/PreGameState.cpp
@@ -27,7 +27,7 @@ PreGameState::Update(const CU::Time& aDeltaTime, ProxyStateStack& aStateStack)
 {
 	myTextBox.Update();
 
-	if (IsometricInput::GetAnyMouseButtonPressed() == true)
+	if (IsometricInput::GetAnyKeyPressed() == true || IsometricInput::GetAnyMouseButtonPressed() == true)
 	{
 		return eStackReturnValue::eDeleteSubstate;
 	}
